ca9e9c7d600b905fb80d6363620acdff34773fde
James Jasper Villamor <repsajsemaj@gmail.com>
2012-11-30 20:54:32 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ite106::Application.config.secret_token = 'b2020c74706719ee01a3b66ff38ef8e8782b8e65912432682eb8ad4638d2158a7133f91a2e41708e4bab256acc0d133c29223deae21ee5fcd04bce4a6e1a1446'
